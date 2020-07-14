2979d4f7a5275251d387a01f2f0d29005db7624f
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-14 14:14:51 +0100

    add history array to store deposit transactions

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,13 +1,15 @@
 class Transaction
   attr_reader :balance, :history
 
+  DEFAULT_BALANCE = 0
   def initialize
-    @balance = 0
+    @balance = DEFAULT_BALANCE
     @history = []
   end
 
   def deposit(amount)
     @balance += amount
+    @history.push({credit: amount})
   end
 
   def withdraw(amount)
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,6 +2,10 @@ require 'transaction'
 
 describe Transaction do
 
+  let(:transaction1) do
+    { credit: '10' }
+  end
+
   it 'is initialized with a starting balance of zero' do
     expect(subject.balance).to eq(0)
   end
@@ -17,6 +21,11 @@ describe Transaction do
   it 'can top up the balance' do
     expect{ subject.deposit 1 }.to change{ subject.balance }.by 1
   end
+
+  it 'adds the deposit transaction to the history' do 
+    subject = Transaction.new
+    subject.deposit(10)
+    expect(subject.history).to eq([{credit:10}])
 end
 
   describe '#withdraw' do
